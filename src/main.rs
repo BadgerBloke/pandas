1d0d47914e9ebbf4a3cb80e5583fab6a26c0263c
Mukesh Singh <camukeshkumarsingh@gmail.com>
2022-10-19 23:16:38 +0530

    Crate Initiated

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+/// This crate is inspired from pandas which is a software library written for the Python programming language for data manipulation and analysis. In particular, it offers data structures and operations for manipulating numerical tables and time series. It is free software released under the Apache License 2.0.
+///
+/// # Under Development
+///
+/// This library is under development and will be released with multiple features in future.
+///
+
+fn main() {
+    println!("Hello, world! This library is under development.");
+}
